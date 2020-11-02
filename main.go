6977b54b81792542bb0f4dd3924092254868701a
gengxiaoyun <gengxiaoyun@shu.edu.cn>
2020-11-02 16:32:13 +0800

    commit

--- main.go
+++ main.go
@@ -12,6 +12,18 @@ import (
 	"bufio"
 )
 
+
+var(
+	destfile = "/home/gengxy/mysql01/"
+	srcfile = "/home/gengxy/mysql/mysql-5.7.31-linux-glibc2.12-x86_64.tar.gz"
+	mysql_dir = destfile+"mysql-5.7.31-linux-glibc2.12-x86_64/"
+	password = "mysql"
+	src = mysql_dir+"support-files/mysql.server"
+	dest = "/etc/init.d/mysql"
+	src2 = "/etc/init.d/mysql.mdf"
+)
+
+
 func createfile(name string) (*os.File,error) {
 	err:=os.MkdirAll(string([]rune(name)[0:strings.LastIndex(name,"/")]),0755)
 	if err!=nil{
@@ -19,7 +31,7 @@ func createfile(name string) (*os.File,error) {
 	}
 	return os.Create(name)
 }
-
+// unzip
 func untargz(srcfile string, destfile string) error {
 	fr, err := os.Open(srcfile)
 	if err != nil {
@@ -63,6 +75,7 @@ func untargz(srcfile string, destfile string) error {
 	return nil
 }
 
+// os.exec command
 func Cmd(command,password string) {
 	cmd:=exec.Command("/bin/bash","-c",command)
 	err:=cmd.Start()
@@ -85,34 +98,10 @@ func Cmd(command,password string) {
 	w.Close()
 	grep.Wait()
 	io.Copy(os.Stdout,&buffer)
+
 }
 
-func Cmdfile(command,password string,f func(string)) {
-	cmd:=exec.Command("/bin/bash","-c",command)
-	err:=cmd.Start()
-	if err != nil{
-		fmt.Println(err.Error())
-	}
-	cmd.Wait()
-	ps:=exec.Command("echo",password)
-	grep:=exec.Command("passwd","-stdin",password)
-	r,w:=io.Pipe()
-	defer r.Close()
-	defer w.Close()
-	ps.Stdout=w
-	grep.Stdin=r
-	var buffer bytes.Buffer
-	grep.Stdout=&buffer
-	_=ps.Start()
-	_=grep.Start()
-	ps.Wait()
-	w.Close()
-	grep.Wait()
-	io.Copy(os.Stdout,&buffer)
-	f("/etc/init.d/mysql")
-}
-
-
+//change file --/etc/init.d/mysql
 func readline(filename string) {
 	f,err:=os.Open(filename)
 	if err!=nil{
@@ -150,6 +139,7 @@ func readline(filename string) {
 
 
 	}
+
 	//err1:=os.Remove(filename)
 	//if err1!=nil{
 	//	fmt.Println(err1.Error())
@@ -159,9 +149,7 @@ func readline(filename string) {
 	//	fmt.Println(err2.Error())
 	//}
 
-
 }
-
 
 //out,err:=cmd.Output()
 //if err != nil{
@@ -169,13 +157,41 @@ func readline(filename string) {
 //}
 //return string(out),err
 
+// mysql initialize
+func Cmdinit(command,password string) {
+	cmd:=exec.Command("/bin/bash","-c",command)
+	cmd.Dir="/home/gengxy/mysql01/mysql-5.7.31-linux-glibc2.12-x86_64/bin/"
+	err:=cmd.Start()
+	if err != nil{
+		fmt.Println(err.Error())
+	}
+	cmd.Wait()
+	ps:=exec.Command("echo",password)
+	grep:=exec.Command("passwd","-stdin",password)
+	r,w:=io.Pipe()
+	defer r.Close()
+	defer w.Close()
+	ps.Stdout=w
+	grep.Stdin=r
+	var buffer bytes.Buffer
+	grep.Stdout=&buffer
+	_=ps.Start()
+	_=grep.Start()
+	ps.Wait()
+	w.Close()
+	grep.Wait()
+	io.Copy(os.Stdout,&buffer)
 
+}
+
+// output error message
 func checkerr(command string) {
 	cmd:=exec.Command("/bin/bash","-c",command)
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout=&out
 	cmd.Stderr=&stderr
+	cmd.Dir="/home/gengxy/mysql01/mysql-5.7.31-linux-glibc2.12-x86_64/bin/"
 	err:=cmd.Run()
 	if err != nil{
 		fmt.Println(fmt.Sprint(err)+": "+stderr.String())
@@ -186,66 +202,60 @@ func checkerr(command string) {
 
 
 func main() {
-	// file read
-	//destfile:="/home/gengxy/mysql01/"
-	//srcfile:="/home/gengxy/mysql/mysql-5.7.31-linux-glibc2.12-x86_64.tar.gz"
-	//mysql_dir:=destfile+"mysql-5.7.31-linux-glibc2.12-x86_64/"
-	//password:="mysql"
-
-	//untargz(srcfile,destfile)
-	//fmt.Println("un tar.gz ok")
+	// unzip
+	untargz(srcfile,destfile)
+	fmt.Println("un tar.gz ok")
 
 	// add_user_chown_chmod
-	//user:="sudo useradd mysql"
-	//chown1:="sudo chown -R mysql:mysql "+mysql_dir
-	//chown2:="sudo chown -R mysql "+mysql_dir
-	//chmod:="sudo chmod -R 755 "+mysql_dir
-	//Cmd(user,password)
-	//Cmd(chown1,password)
-	//Cmd(chown2,password)
-	//Cmd(chmod,password)
+	user := "sudo useradd mysql"
+	chown1 := "sudo chown -R mysql:mysql "+mysql_dir
+	chown2 := "sudo chown -R mysql "+mysql_dir
+	chmod := "sudo chmod -R 777 "+mysql_dir
+	Cmd(user,password)
+	Cmd(chown1,password)
+	Cmd(chown2,password)
+	Cmd(chmod,password)
 
 	// install_libaio
-	//slibaio:="sudo yum search libaio"
-	//ilibaio:="sudo yum install libaio"
-	//yum(slibaio)
-	//yum(ilibaio)
+	ilibaio := "sudo apt-get install libaio"
+	Cmd(ilibaio,password)
 
 	// copy_file
-	//src := mysql_dir+"support-files/mysql.server"
-	//dest := "/etc/init.d/mysql"
-	//sercmd := fmt.Sprintf(`sudo cp "%s" "%s"`,src,dest)
-	//Cmd(sercmd,password)
+	sercmd := fmt.Sprintf(`sudo cp "%s" "%s"`,src,dest)
+	Cmd(sercmd,password)
 
 	// add_path
-	//cmdstr1:="sudo chmod -R a=rwx /etc/init.d/mysql"
-	//Cmd(cmdstr1,password)
-	//cmdstr2:="sudo touch /etc/init.d/mysql.mdf"
-	//Cmd(cmdstr2,password)
-	//cmdstr3:="sudo chmod -R a=rwx /etc/init.d/mysql.mdf"
-	//Cmd(cmdstr3,password)
-	//readline("/etc/init.d/mysql")
-	//src2 := "/etc/init.d/mysql.mdf"
-	//dest2 := "/etc/init.d/mysql"
-	//cmdremove := fmt.Sprintf(`sudo mv "%s" "%s"`,src2,dest2)
-	//Cmd(cmdremove,password)
+	cmdstr1 := "sudo chmod -R a=rwx /etc/init.d/mysql"
+	cmdstr2 := "sudo touch /etc/init.d/mysql.mdf"
+	cmdstr3 := "sudo chmod -R a=rwx /etc/init.d/mysql.mdf"
+	Cmd(cmdstr1,password)
+	Cmd(cmdstr2,password)
+	Cmd(cmdstr3,password)
+	readline(dest)
+	cmdremove := fmt.Sprintf(`sudo mv "%s" "%s"`,src2,dest)
+	Cmd(cmdremove,password)
 
 	// initialize
-	//cmdstr4:=mysql_dir+"bin/mysqld --initialize --user=mysql --basedir="+mysql_dir+" --datadir="+mysql_dir+"data"
-	//checkerr(cmdstr4)
+	cmdstr4:="./mysqld --initialize --user=mysql --basedir="+mysql_dir+" --datadir="+mysql_dir+"data"
+	////checkerr(cmdstr4)
+	Cmdinit(cmdstr4,password)
 
-	//b,err:=cmd.Output()
-	//fmt.Println(string(b),err)
+	cmdstr5:="./mysql_ssl_rsa_setup --datadir="+mysql_dir+"data"
+	////checkerr(cmdstr5)
+	Cmdinit(cmdstr5,password)
 
-	//cmdstr5:=mysql_dir+"bin/mysql_ssl_rsa_setup --datadir="+mysql_dir+"data"
-	//checkerr(cmdstr5)
-	//cmdstr6:=mysql_dir+"bin/mysqld_safe --user=mysql &"
-	//checkerr(cmdstr6)
-	//cmdstr7:="ps -ef|grep mysql"
+	cmdstr6:="./mysqld_safe --user=mysql &"
+	////checkerr(cmdstr6)
+	Cmdinit(cmdstr6,password)
+
+	cmdstr7:="ps -ef|grep mysql"
 	//checkerr(cmdstr7)
-	//
-	//cmdstr8:="mysql -uroot -p"
-	//checkerr(cmdstr8)
+	Cmd(cmdstr7,password)
+
+	//login
+	cmdstr8:="mysql -uroot -p"
+	Cmd(cmdstr8,password)
+
 }
 
 
