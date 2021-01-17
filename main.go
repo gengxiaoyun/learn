5ddb4d68d70db94d6c9453dde4b142a84c7a4c38
gengxiaoyun <gengxiaoyun@shu.edu.cn>
2021-01-17 16:18:06 +0800

    newCommit

--- main.go
+++ main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"flag"
-	"runtime"
-	"os"
+	"fmt"
 	"github.com/gengxiaoyun/learn/prepare"
 	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+	"runtime"
 )
 
 func main() {
 	var (
-		err error
+		err         error
 		logFileName = flag.String("./log", "InstallMysql.log", "Log file name")
 	)
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	flag.Parse()
 	//set logfile Stdout
+	_, err = os.Stat(*logFileName)
+	if err == nil {
+		err = os.Remove(*logFileName)
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}
 	logFile, logErr := os.OpenFile(*logFileName, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
 	if logErr != nil {
 		fmt.Println("Fail to find", *logFile, "InstallMysql start Failed")
@@ -29,15 +36,14 @@ func main() {
 	log.Printf("Log start! File:%v \n", "InstallMysql.log")
 
 	r := gin.Default()
-	r.GET("/",func(c *gin.Context){
-		c.JSON(200,c.QueryArray("address"))
-		err = prepare.StartMysql(c.QueryArray("address"),c.Query("user"),c.Query("password"))
-
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, c.QueryArray("address"))
+		err = prepare.StartMysql(c.QueryArray("address"), c.Query("user"), c.Query("password"))
+		if err != nil {
+			log.Println(err.Error())
+		}
+		log.Println("AutoInStallMysql succeeded!")
 	})
 	r.Run(":8080")
 
-	if err != nil{
-		log.Println(err.Error())
-	}
-	log.Println("AutoInStallMysql succeeded!")
 }
